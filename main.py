9e169c3a552907796b8f5c33c30fd157f49d50f6
NU2M <mybdye@gmail.com>
2022-03-25 19:55:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -161,7 +161,6 @@ def barkPush(body):
 
 options = Options()
 #options.add_argument("--start-maximized") #open Browser in maximized mode
-options.set_headless
 options.add_argument("--no-sandbox") #bypass OS security model
 options.add_argument("--disable-dev-shm-usage") #overcome limited resource problems
 options.add_argument("--headless")
